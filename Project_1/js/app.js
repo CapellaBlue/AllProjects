fb65a84d419eee989395f397cd5190dd140bbee1
Amanda Capella <mrs.acapella@gmail.com>
2017-03-27 21:17:03 -0700

    Added feedback button and versus computer option. versus computer does not work

--- Project_1/js/app.js
+++ Project_1/js/app.js
@@ -2,15 +2,18 @@
 
 //==============================================================================
 //----------------------ARRAYS FOR FEEDBACK CODE & GUESSES----------------------
+//--------------------------& Global Elements-----------------------------------
 //==============================================================================
     //array to hold the codemaker's selection
     var code = [];
+    //array to hold the most recent guess by the code breaker
     var guess = [];
-    // var guess1 = [];
-    // var guess2 = [];
-    // var guess3 = [];
-    // var guess4 = [];
+    //an array to store feedback for computer if I get to making the computer be the code breaker
     var feedback = [];
+
+    //player scores;
+    var playerOneScore = 0;
+    var playerTwoScore = 0;
 
 
 //==============================================================================
@@ -27,9 +30,13 @@
     //feedback buttons!
     var $black = $('#black');
     var $gray = $('#gray');
+    var $feedbackDone = $('#feedback-done-button');
 
     //display code button
     var $displayCodeBtn = $('#displayCode');
+
+    var $versusComputerBtn = $('#versus-computer-btn');
+    var $twoPlayerBtn =$('#two-player-btn');
 
 
 //==============================================================================
@@ -37,7 +44,13 @@
 //==============================================================================
 
     var start = function(){
-      codeMakerFirstTurn();
+      var twoPlayerMode = function(){
+        codeMakerFirstTurn();
+      };
+
+      $twoPlayerBtn.on('click', twoPlayerMode);
+      // $versusComputerBtn.on('click', versusComputerMode);
+
     };
 
     console.log("You're doing fine");
@@ -46,6 +59,9 @@
 //==============================================================================
     //when button is clicked....store in code-storage array and append
     var codeMakerFirstTurn = function(){
+      $('.player-mode').css('display', 'none');
+      $('.feedback-buttons').css('visibility', 'visible');
+      $('#feedback-done-button').css('visibility', 'visible');
       if(code.length < 4){
         var storeCode = function(){
           var $selectedCode = $('<div>').attr('id',$(this).attr('id'));
@@ -86,9 +102,7 @@
       guess = [];
       console.log("still doing swell:)");
       if(guess.length < 4){
-        console.log('something');
         var storeGuess = function(){
-          console.log('storeGuess');
           var $selectedGuess = $('<div>').attr('id',$(this).attr('id'));
           $selectedGuess.addClass('guessed-square');
           guess.push($(this).attr('id'));
@@ -102,11 +116,6 @@
             $('#third-round-guess').append($selectedGuess);
           } else if($('#first-round-guess > div').length == 4 && $('#second-round-guess > div').length == 4 && $('#third-round-guess > div').length == 4 && $('#fourth-round-guess > div').length < 4 ){
             $('#fourth-round-guess').append($selectedGuess);
-          // } else if($('#first-round-guess > div').length == 4 && $('#second-round-guess > div').length == 4 &&
-          // $('#third-round-guess > div').length == 4 &&
-          // $('#fourth-round-guess > div').length == 4) {
-          //
-          // }
             };
 
 
@@ -140,21 +149,32 @@
 //---------------------------CODE MAKER GIVES FEEDBACK--------------------------
 //==============================================================================
 
-  var feedbackTime = function(){
-    feedback = [];
-    var giveFeedback = function (){
-      feedback.push($(this).attr('id'));
-      var $feedback = $('<div>').attr('id', $(this).attr('id')).addClass('feedback-square');
-      $('#first-round-feedback').append($feedback);
+var feedbackTime = function(){
+  feedback = [];
 
-      if($('#first-round-feedback > div').length < 5){
+
+    for (var i = 0; i < code.length; i++) {
+      if(code[i] == guess[i] && code[i + 1] == guess[i + 1] && code[i + 2] == guess[i + 2] && code[i + 3] == guess[i + 3]){
+      $('.instruction').text("Code Breaker wins round!").css('color','white').css('font-size', '50px');//maybe add a reset button here.
+      playerOneScore += .25;
+      console.log(playerOneScore);
+    };
+  };
+
+
+  var giveFeedback = function (){
+    feedback.push($(this).attr('id'));
+    var $feedback = $('<div>').attr('id', $(this).attr('id')).addClass('feedback-square');
+
+
+      if($('#first-round-feedback > div').length < 4){
         $('#first-round-feedback').append($feedback);
-      } else if($('#first-round-feedback > div').length == 5 && $('#second-round-feedback > div').length < 4){
+      } else if($('#first-round-feedback > div').length == 4 && $('#second-round-feedback > div').length < 4){
         $('#second-round-feedback').append($feedback);
-      } else if($('#first-round-feedback > div').length == 5 && $('#second-round-feedback > div').length == 4 &&
+      } else if($('#first-round-feedback > div').length == 4 && $('#second-round-feedback > div').length == 4 &&
       $('#third-round-feedback > div').length < 4){
         $('#third-round-feedback').append($feedback);
-      } else if($('#first-round-feedback > div').length == 5 && $('#second-round-feedback > div').length == 4 &&
+      } else if($('#first-round-feedback > div').length == 4 && $('#second-round-feedback > div').length == 4 &&
       $('#third-round-feedback > div').length == 4 &&
       $('#fourth-round-feedback > div').length < 4 ){
         $('#fourth-round-feedback').append($feedback);
@@ -170,11 +190,11 @@
 
       };
     };
-
-    console.log(feedback);
     $black.on('click', giveFeedback);
     $gray.on('click', giveFeedback);
   };
+
+
 
 
 //==============================================================================
@@ -182,7 +202,6 @@
 //==============================================================================
 
   var displayCode = function(){
-    console.log(code);
     $displayCodeBtn.on('click', hideCode);
     $displayCodeBtn.text("Hide Code");
     for (var i = 0; i < code.length; i++) {
